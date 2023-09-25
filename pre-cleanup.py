465d68d4cc402ef25e240e26556381fe8609cd21
Joseph Villarreal Lopez <joseph.villarreal@66degrees.com>
2023-09-25 15:01:52 -0600

    feat: add PDF processing functionality

--- pre-cleanup.py
+++ pre-cleanup.py
@@ -1,20 +1,25 @@
+# Importing required libraries
 import argparse
 import os
 import re
 from bs4 import BeautifulSoup
-from num2words import num2words
 from nltk.tokenize import sent_tokenize
 
+# Function to process all text files in a directory
 def process_files_in_directory(directory_path):
     try:
+        # Loop through each file in the directory
         for filename in os.listdir(directory_path):
+            # Check if the file is a text file
             if filename.endswith('.txt'):
+                # Construct the full filepath
                 filepath = os.path.join(directory_path, filename)
 
+                # Open and read the file
                 with open(filepath, 'r', encoding='utf-8') as file:
                     text = file.read()
 
-                # Use BeautifulSoup to clean up HTML tags
+                # Use BeautifulSoup to clean up HTML tags in the text
                 soup = BeautifulSoup(text, 'html.parser')
                 text = soup.get_text()
 
@@ -30,22 +35,34 @@ def process_files_in_directory(directory_path):
                 text = text.replace('St.', 'Street')
                 text = text.replace('pp.', 'pages')
                 text = text.replace('p.', 'page')
+                # Remove hyphens between words
                 text = re.sub(r'(?<=[a-zA-Z])\s*[-‐]\s*(?=[a-zA-Z])', '', text)
+                # Replace multiple spaces with a single space
                 text = re.sub(r' +', ' ', text)
+                # Remove space before commas
                 text = re.sub(r' ,', ',', text)
+                # Replace '&' with 'and'
                 text = text.replace('&', 'and')
 
+                # Write the cleaned up text back to the file
                 with open(filepath, 'w', encoding='utf-8') as file:
                     file.write(text)
                     print(filepath)
 
+        # Print a success message when all files have been processed
         print('All files have been processed.')
     except Exception as e:
+        # Print an error message if something goes wrong
         print(f'An error occurred: {e}')
 
+
+# Main function to handle command line arguments and call the processing function
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process text files in a directory')
+    parser = argparse.ArgumentParser(description='Process text files in a dir')
+    # Add a command line argument for the directory path
     parser.add_argument('--f', help='Directory path', type=str, required=True)
 
+    # Parse the command line arguments
     args = parser.parse_args()
+    # Call the processing function with the directory path argument
     process_files_in_directory(args.f)
--- pre-pdf.py
+++ pre-pdf.py
@@ -1,71 +1,98 @@
+# Importing required libraries
 import argparse
 import os
 from PyPDF2 import PdfReader
 import fitz
 
+# Function to split pages from a PDF and save as text files
 def split_pages_from_pdf(pdf_path, output_folder, pages):
     try:
+        # Open the PDF file
         with open(pdf_path, 'rb') as file:
             reader = PdfReader(file)
+            # If no specific pages are provided, process all pages
             if not pages:
                 pages = list(range(len(reader.pages)))
 
+            # Loop through each page
             for page_num in pages:
                 page = reader.pages[page_num]
+                # Extract text from the page
                 text = page.extract_text()
                 cleaned_text = text  # text.replace("", "")
 
+                # Construct the output file path
                 output_file_path = os.path.join(output_folder, f'pag_{page_num+1}.txt')
+                # Write the extracted text to the output file
                 with open(output_file_path, 'w') as output_file:
                     output_file.write(cleaned_text)
                 print(f'Text from pag {page_num+1} extracted and saved to {output_file_path}')
     except Exception as e:
         print(f'An error occurred: {e}')
 
+
+# Function to convert pages from a PDF to images
 def pdf_to_images(pdf_path, output_folder, pages):
     try:
+        # Open the PDF document
         doc = fitz.open(pdf_path)
+        # If no specific pages are provided, process all pages
         if not pages:
             pages = list(range(len(doc)))
 
+        # Loop through each page
         for page_num in pages:
             page = doc[page_num]
+            # Convert the page to an image
             img = page.get_pixmap()
+            # Save the image to the output folder
             img.save(os.path.join(output_folder, f'pag_{page_num}.png'))
         print(f'PDF converted to images and saved to {output_folder}')
     except Exception as e:
         print(f'An error occurred: {e}')
 
+
+# Function to parse the pages argument
 def parse_pages_argument(pages_str):
+    # If a range of pages is provided
     if '-' in pages_str:
         start, end = map(int, pages_str.split('-'))
         return list(range(start-1, end))
+    # If specific pages are provided
     elif ',' in pages_str:
         return list(map(lambda x: int(x) - 1, pages_str.split(',')))
     else:
         return [int(pages_str) - 1]
 
+
+# Main function to handle command line arguments and call the appropriate function
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PDF Processing')
+    # Add command line arguments for the pages, PDF file path, output folder, and mode
     parser.add_argument('--p', help='Pages to be processed (1,2,3 or 1-5)', type=str)
     parser.add_argument('--f', help='PDF file path', type=str, required=True)
     parser.add_argument('--o', help='Output folder', type=str, required=True)
     parser.add_argument('--m', help='Mode (split/photo)', type=str, required=True)
 
+    # Parse the command line arguments
     args = parser.parse_args()
 
+    # Check if the mode is valid
     if args.m not in ['split', 'photo']:
         print("Invalid mode. Use 'split' or 'photo'")
         exit(1)
 
+    # Parse the pages argument if provided
     if args.p:
         pages = parse_pages_argument(args.p)
     else:
         pages = None
 
+    # Create the output folder if it doesn't exist
     if not os.path.exists(args.o):
         os.makedirs(args.o)
 
+    # Call the appropriate function based on the mode
     if args.m == 'split':
         split_pages_from_pdf(args.f, args.o, pages)
     elif args.m == 'photo':
